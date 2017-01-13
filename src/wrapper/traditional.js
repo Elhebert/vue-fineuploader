3ff92a1e2e25e081d4fd0ba74779810bc9b211f8
Elhebert <ehebert@hotmail.be>
2017-01-14 00:44:19 +0100

    [refactor] Used react-vue wrappers

---
+++ src/wrapper/traditional.js
@@ -0,0 +1,14 @@
+import BaseWrapper from './base-wrapper'
+import qq from 'fine-uploader/lib/core'
+import { traditional as callbackNames } from './callback-names'
+
+export default class FineUploaderTraditional extends BaseWrapper {
+  constructor ({ options }) {
+    super({
+      callbackNames,
+      options,
+      qq,
+      type: 'traditional'
+    })
+  }
+}
