b7fbbf767ddaf73417da282bf150ec40515e7c93
Elhebert <elhebert@hotmail.be>
2017-01-14 21:40:02 +0100

    [refactor] Ported react cancel-button component to vue

---
+++ src/components/cancel-button.vue
@@ -0,0 +1,77 @@
+<template>
+  <button aria-label="cancel"
+    class="vue-fine-uploader-cancel-button" }
+    :disabled="!this.state.cancelable"
+    :onClick="this.state.cancelable && this._onClick">
+      <slot></slot>
+  </button>
+</template>
+
+<script>
+  const isCancelable = status => {
+    return [
+      'delete failed',
+      'paused',
+      'queued',
+      'retrying upload',
+      'submitted',
+      'uploading',
+      'upload failed'
+    ].indexOf(status) >= 0
+  }
+
+  export default {
+
+    props: {
+      id: {
+        type: Number,
+        require: true
+      },
+      onlyRenderIfCancelable: {
+        type: Boolean,
+        default: true
+      },
+      uploader: {
+        type: Object,
+        require: true
+      }
+    },
+
+    data () {
+      return {
+        state: {
+          cancelable: true
+        }
+      }
+    },
+
+    mounted () {
+      this.uploader.on('statusChange', this._onStatusChange)
+    },
+
+    beforeDestroy () {
+      this._unmounted = true
+      this._unregisterStatusChangeHandler()
+    },
+
+    methods: {
+      _onStatusChange: (id, oldStatus, newStatus) => {
+        if (id === this.id && !this._unmounted) {
+          if (!isCancelable(newStatus) && this.state.cancelable) {
+            this.setState({ cancelable: false })
+          } else if (isCancelable(newStatus) && !this.state.cancelable) {
+            this.setState({ cancelable: true })
+          } else if (newStatus === 'deleted' || newStatus === 'canceled') {
+            this._unregisterStatusChangeHandler()
+          }
+        }
+      },
+
+      _onClick: () => this.uploader.methods.cancel(this.id),
+
+      _unregisterStatusChangeHandler: () => {
+        this.uploader.off('statusChange', this._onStatusChange)
+      }
+    }
+  }
+</script>
