aa6af42c879e684f5b383e98b3f5654572cdd341
Elhebert <ehebert@hotmail.be>
2017-01-14 01:38:45 +0100

    [refactor] Ported react dropzone component to vue

---
+++ src/components/dropzone.vue
@@ -0,0 +1,69 @@
+<template>
+  <div :class="{{ fine-uploader-dropzone-container }}" ref="dropZone">
+      {{ this.children }}
+  </div>
+</template>
+
+<style></style>
+
+<script>
+  import qq from 'fine-uploader/lib/dnd'
+
+  export default {
+    props: {
+      children: { type: Node },
+      dropActiveClassName: { type: String, default: 'vue-fine-uploader-dropzone-active' },
+      element: { type: String },
+      multiple: { type: Boolean },
+      onDropError: { type: Function },
+      onProcessingDroppedFiles: { type: Function },
+      onProcessingDroppedFilesComplete: { type: Function },
+      uploader: { type: Object, required: true }
+    },
+
+    mounted() {
+      this._registerDropzone()
+    }
+
+    updated() {
+      this._registerDropzone()
+    }
+
+    beforeDestroy() {
+      this._qqDropzone && this._qqDropzone.dispose()
+    }
+
+    methods: {
+      _onDropError(errorCode, errorData) {
+        console.error(errorCode, errorData)
+
+        this.onDropError && this.onDropError(errorCode, errorData)
+      },
+
+      _onProcessingDroppedFilesComplete(files) {
+        this.uploader.methods.addFiles(files)
+
+        this.onProcessingDroppedFilesComplete) && this.onProcessingDroppedFilesComplete(files)
+      },
+
+      _registerDropzone() {
+        this._qqDropzone && this._qqDropzone.dispose()
+
+        const dropzoneEl = this.element || this.refs.dropZone
+
+        this._qqDropzone = new qq.DragAndDrop({
+          allowMultipleItems: !!this.multiple,
+          callbacks: {
+            dropError: this._onDropError.bind(this),
+            processingDroppedFiles: this.onProcessingDroppedFiles || function() {},
+            processingDroppedFilesComplete: this._onProcessingDroppedFilesComplete.bind(this)
+          },
+          classes: {
+            dropActive: this.dropActiveClassName || ''
+          },
+          dropZoneElements: [dropzoneEl]
+        })
+      }
+    }
+  }
+</script>
