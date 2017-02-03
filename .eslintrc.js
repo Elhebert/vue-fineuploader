71e8e5a7a221f5491d6d37e1303cb727b3bf8e7b
Elhebert <elhebert@hotmail.be>
2017-02-03 19:09:30 +0100

    [refactor] Use fine-uploader-wrappers
    vue-fineupload now depends on fine-uploader-wrappers
    
    Breaking changes:
    - Traditional, S3, and Azure wrappers must be imported from 'fine-uploader-wrappers'
    - Components are now at the root of the package.
    
    Also, <dropzone /> is now the default export for this package.
    
    closes #17

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,28 +5,15 @@ module.exports = {
         sourceType: 'module',
     },
     extends: 'standard',
-    // required to lint *.vue files
     plugins: [
         'html',
     ],
     'env': {
         'browser': true,
     },
-    // check if imports actually resolve
-    'settings': {
-        'import/resolver': {
-            'webpack': {
-                'config': 'build/webpack.base.conf.js'
-            },
-        },
-    },
-    // add your custom rules here
     'rules': {
-        // allow paren-less arrow functions
         'arrow-parens': 0,
-        // allow async-await
         'generator-star-spacing': 0,
-        // allow debugger during development
         'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     },
 };
